0d0041ec3f05483b08ceac23f39ead94614f8a5a
Nathan <nathandebeech@berkeley.edu.com>
2025-09-16 21:54:17 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,71 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#  Git is the technology. GitHub is the website that is used as remote repository. Git Bash is the interface for windows.
+
+# 2) What’s the difference between the terminal and the command line?
+# The terminal is the software specific for your computer, the command line is the line within the terminal
+
+# 3) How does Windows PowerShell differ from Git Bash?
+#  They require completey different languages to utilize the respective systems.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Python is the coding language, Anaconda is the system that allows you to run python, conda allows you to update python
+
+# 5) What is VS Code? 
+# The system that allows you to write code easily. What I am currently using!
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# They are two seperate applications. Notebook is simplier. Lab allows for greater range of functionality.
+
+# 7) What does ~/ mean?
+# You are in the parent directory
+
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path is the overarching path to leading to the current directory whereas the relative path skips the previous directories and only uses the current directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# absolute: python_decal_fa25/nathan/course_assignments/homework2       relative: course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ../
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# deletes the directory
+
+# 12) What do the following commands do?
+# git add
+# begins to save file
+# git commit
+# saves file on the local repositroy
+# git push
+# saves file onto remote repository
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . adds all current files whereas git addd <file> only adds the specific file you type.
+
+# 14) What do "git status" and "git log -1" do?
+# Provides you with the history of how you have used git
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# cloning copies the information, pulling saves the information onto the local repository from the remote
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# I created a folder instead of a file when creating homework2. I attempted to create a new file but messed more things up! I ended up asking for help during class.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# On question 9 of the this homework, it implies that course_assignments is inside of the yourname directory, but that is not true for me. Is this okay?
+
+# 18) Tell me a fun fact!
+# Farts have been measured to travel around 10 feet per second
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+
+
+print(2 ** 4.3) # The two multiplication symbols indicate an exponet.
